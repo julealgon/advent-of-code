cc74a17977ce58c0c33143b8dbe76641b0a01355
Juliano Leal Goncalves <jgoncalves@affinitiv.com>
2022-12-01 12:39:01 -0300

    ✨ Add day1 part2 logic

--- 2022/julealgon.AdventOfCode._2022._1-CalorieCounting/Program.cs
+++ 2022/julealgon.AdventOfCode._2022._1-CalorieCounting/Program.cs
@@ -1,7 +1,8 @@
 ﻿using julealgon.AdventOfCode.Extensions;
 using MoreLinq;
 
-Part1();
+//Part1();
+Part2();
 
 void Part1()
 {
@@ -12,4 +13,17 @@ void Part1()
         .Max();
 
     Console.Write(maximumCalorieTotal);
+}
+
+void Part2()
+{
+    var totalCaloriesTop3Elves = Console.In
+        .IterateLines()
+        .Split(string.Empty)
+        .Select(elf => elf.Select(calorie => Convert.ToInt32(calorie)).Sum())
+        .OrderDescending()
+        .Take(3)
+        .Sum();
+
+    Console.Write(totalCaloriesTop3Elves);
 }
