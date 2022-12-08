907bf524f35fd16edca5fdc2400f00d31a99ddc0
Juliano Leal Goncalves <jgoncalves@affinitiv.com>
2022-12-08 08:21:32 -0300

    ✨ Add day7 part1 and 2 logic

---
+++ julealgon.AdventOfCode._2022._7-NoSpaceLeftOnDevice/ElfFile.cs
@@ -0,0 +1,53 @@
+﻿using OneOf;
+using static julealgon.AdventOfCode._2022._7_NoSpaceLeftOnDevice.ElfConsoleCommand;
+
+namespace julealgon.AdventOfCode._2022._7_NoSpaceLeftOnDevice;
+
+internal readonly record struct ElfFileSystem
+{
+    public ElfFileSystem()
+    {
+        RootDirectory = new();
+    }
+
+    public ElfDirectory RootDirectory { get; }
+}
+
+internal readonly record struct ElfFile
+{
+    public required string Name { get; init; }
+
+    public required int Size { get; init; }
+}
+
+internal sealed record class ElfDirectory
+{
+    public ElfDirectory()
+    {
+        Files = new List<ElfFile>();
+        Directories= new List<ElfDirectory>();
+    }
+
+    public ICollection<ElfFile> Files { get; }
+
+    public ICollection<ElfDirectory> Directories { get; }
+
+    public ElfDirectory? Parent { get; set; }
+
+    public string? Name { get; set; }
+
+    public int Size => Files.Sum(f => f.Size) + Directories.Sum(d => d.Size);
+}
+
+[GenerateOneOf]
+public sealed partial class ElfConsoleCommand : OneOfBase<ListDirectoryContents, ChangeDirectory>
+{
+    public sealed class ListDirectoryContents
+    {
+    }
+
+    public sealed class ChangeDirectory
+    {
+    }
+}
+
---
+++ julealgon.AdventOfCode._2022._7-NoSpaceLeftOnDevice/Program.cs
@@ -0,0 +1,136 @@
+﻿using julealgon.AdventOfCode._2022._7_NoSpaceLeftOnDevice;
+using julealgon.AdventOfCode.Extensions;
+using MoreLinq;
+using System.Text.RegularExpressions;
+
+//Part1();
+Part2();
+
+void Part1()
+{
+    var fileSystem = new ElfFileSystem();
+    var currentDirectory = fileSystem.RootDirectory;
+
+    foreach (string line in Console.In.IterateLines())
+    {
+        if (line is "$ cd /")
+        {
+            currentDirectory = fileSystem.RootDirectory;
+            continue;
+        }
+
+        if (line is "$ cd ..")
+        {
+            currentDirectory = currentDirectory!.Parent!;
+            continue;
+        }
+
+        if (line.StartsWith("$ cd"))
+        {
+            var directoryName = line.Split(' ').Last();
+            currentDirectory = currentDirectory!.Directories.Single(d => d.Name == directoryName);
+            continue;
+        }
+
+        if (line is "$ ls")
+        {
+            continue;
+        }
+
+        if (line.StartsWith("dir "))
+        {
+            var directoryName = line.Split(' ').Last();
+            currentDirectory.Directories.Add(new() { Name = directoryName, Parent = currentDirectory });
+            continue;
+        }
+
+        var fileSize = line.Split(' ').First();
+        var fileName = line.Split(' ').Last();
+        currentDirectory.Files.Add(new() { Name = fileName, Size = int.Parse(fileSize) });
+    }
+
+    var sizeToDelete = MoreEnumerable
+        .TraverseBreadthFirst(fileSystem.RootDirectory, d => d.Directories)
+        .Where(d => d.Size <= 100_000)
+        .Sum(d => d.Size);
+
+    //var sizeToDelete = new Visitor().Visit(fileSystem.RootDirectory).Sum(d => d.Size);
+
+}
+
+void Part2()
+{
+    var fileSystem = new ElfFileSystem();
+    var currentDirectory = fileSystem.RootDirectory;
+
+    foreach (string line in Console.In.IterateLines())
+    {
+        if (line is "$ cd /")
+        {
+            currentDirectory = fileSystem.RootDirectory;
+            continue;
+        }
+
+        if (line is "$ cd ..")
+        {
+            currentDirectory = currentDirectory!.Parent!;
+            continue;
+        }
+
+        if (line.StartsWith("$ cd"))
+        {
+            var directoryName = line.Split(' ').Last();
+            currentDirectory = currentDirectory!.Directories.Single(d => d.Name == directoryName);
+            continue;
+        }
+
+        if (line is "$ ls")
+        {
+            continue;
+        }
+
+        if (line.StartsWith("dir "))
+        {
+            var directoryName = line.Split(' ').Last();
+            currentDirectory.Directories.Add(new() { Name = directoryName, Parent = currentDirectory });
+            continue;
+        }
+
+        var fileSize = line.Split(' ').First();
+        var fileName = line.Split(' ').Last();
+        currentDirectory.Files.Add(new() { Name = fileName, Size = int.Parse(fileSize) });
+    }
+
+    const int totalDiskCapacity = 70_000_000;
+    const int updateDiskRequirement = 30_000_000;
+    int currentDiskUsage = fileSystem.RootDirectory.Size;
+    int freeDiskSpace = totalDiskCapacity - currentDiskUsage;
+    int extraSpaceNeededForUpdate = updateDiskRequirement - freeDiskSpace;
+
+    var sizeToDelete = MoreEnumerable
+        .TraverseBreadthFirst(fileSystem.RootDirectory, d => d.Directories)
+        .OrderBy(d => d.Size)
+        .SkipWhile(d => d.Size < extraSpaceNeededForUpdate)
+        .First().Size;
+}
+
+internal class Visitor
+{
+    public IEnumerable<ElfDirectory> Visit(ElfDirectory value)
+    {
+        if (value.Size <= 100_000)
+        {
+            yield return value;
+        }
+        else
+        {
+            foreach (var child in value.Directories)
+            {
+                foreach (var x in Visit(child))
+                {
+                    yield return x;
+                }
+            }
+        }
+    }
+}
